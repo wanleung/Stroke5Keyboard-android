1bb458c102c8b8f04d9ac7d8937ca2839a879f4f
Wan Leung Wong <wanleung@linkomnia.com>
2012-02-27 02:48:40 +0800

    reformat

--- src/com/linkomnia/android/StrokeFiveKeyboard/StrokeFiveKeyboard.java
+++ src/com/linkomnia/android/StrokeFiveKeyboard/StrokeFiveKeyboard.java
@@ -17,7 +17,7 @@
     You should have received a copy of the GNU General Public License along
     with this program; if not, write to the Free Software Foundation, Inc.,
     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-*/
+ */
 
 package com.linkomnia.android.StrokeFiveKeyboard;
 
@@ -37,81 +37,81 @@ import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputConnection;
 import android.view.inputmethod.InputMethodManager;
 
-public class StrokeFiveKeyboard extends InputMethodService 
-implements KeyboardView.OnKeyboardActionListener {
+public class StrokeFiveKeyboard extends InputMethodService implements
+        KeyboardView.OnKeyboardActionListener {
     /** Called when the activity is first created. */
-	private KeyboardView mInputView;
-	
-	
-	private Stroke5KeyBoard mStroke5;
-	private Stroke5Table mKeyData;
+    private KeyboardView mInputView;
 
-    @Override public void onCreate() {
+    private Stroke5KeyBoard mStroke5;
+    private Stroke5Table mKeyData;
+
+    @Override
+    public void onCreate() {
         super.onCreate();
         mKeyData = new Stroke5Table(this, false);
         mKeyData.open();
     }
-    
-    public void onInitializeInterface () {
-        //ArrayList<String> aaa = mKeyData.searchRecord(",");
-    	mStroke5 = new Stroke5KeyBoard(this, R.xml.stroke5);
-    	//Log.d("WANLEUNG", getFilesDir());
-    	//Log.d("WANLEUNG", aaa.size()+"");
-    	
+
+    public void onInitializeInterface() {
+        // ArrayList<String> aaa = mKeyData.searchRecord(",");
+        mStroke5 = new Stroke5KeyBoard(this, R.xml.stroke5);
+        // Log.d("WANLEUNG", getFilesDir());
+        // Log.d("WANLEUNG", aaa.size()+"");
+
     }
-    
-    public View onCreateInputView () {
-    	mInputView = (KeyboardView) getLayoutInflater().inflate(
-                R.layout.main, null);
+
+    public View onCreateInputView() {
+        mInputView = (KeyboardView) getLayoutInflater().inflate(R.layout.main,
+                null);
         mInputView.setOnKeyboardActionListener(this);
         mInputView.setKeyboard(mStroke5);
-    	return mInputView;
+        return mInputView;
     }
 
-	public void onKey(int primaryCode, int[] keyCodes) {
-		// TODO Auto-generated method stub
-		Log.d("WANLEUNG", "onkey " + primaryCode + " " + keyCodes.toString());
-		ArrayList <String> list = mKeyData.searchRecord(".n.");
-		Log.d("WANLEUNG", list.get(0)+"");
-	}
+    public void onKey(int primaryCode, int[] keyCodes) {
+        // TODO Auto-generated method stub
+        Log.d("WANLEUNG", "onkey " + primaryCode + " " + keyCodes.toString());
+        ArrayList<String> list = mKeyData.searchRecord(".n.");
+        Log.d("WANLEUNG", list.get(0) + "");
+    }
 
-	public void onPress(int primaryCode) {
-		// TODO Auto-generated method stub
-		Log.d("WANLEUNG", "onPress " + primaryCode);
-		
-	}
+    public void onPress(int primaryCode) {
+        // TODO Auto-generated method stub
+        Log.d("WANLEUNG", "onPress " + primaryCode);
 
-	public void onRelease(int primaryCode) {
-		// TODO Auto-generated method stub
-		Log.d("WANLEUNG", "onRelease " + primaryCode);		
-	}
+    }
 
-	public void onText(CharSequence text) {
-		// TODO Auto-generated method stub
-		Log.d("WANLEUNG", "onText " + text.toString());
-	}
+    public void onRelease(int primaryCode) {
+        // TODO Auto-generated method stub
+        Log.d("WANLEUNG", "onRelease " + primaryCode);
+    }
 
-	public void swipeDown() {
-		// TODO Auto-generated method stub
-		Log.d("WANLEUNG", "swipeDown ");
-	}
+    public void onText(CharSequence text) {
+        // TODO Auto-generated method stub
+        Log.d("WANLEUNG", "onText " + text.toString());
+    }
 
-	public void swipeLeft() {
-		// TODO Auto-generated method stub
-		Log.d("WANLEUNG", "swipeLeft ");
-	}
+    public void swipeDown() {
+        // TODO Auto-generated method stub
+        Log.d("WANLEUNG", "swipeDown ");
+    }
 
-	public void swipeRight() {
-		// TODO Auto-generated method stub
-		Log.d("WANLEUNG", "swipeRight ");
-	}
+    public void swipeLeft() {
+        // TODO Auto-generated method stub
+        Log.d("WANLEUNG", "swipeLeft ");
+    }
 
-	public void swipeUp() {
-		// TODO Auto-generated method stub
-		Log.d("WANLEUNG", "swipeUp ");
-	}
-	
-	public void onDestroy() {
-	    this.mKeyData.close();
-	}
+    public void swipeRight() {
+        // TODO Auto-generated method stub
+        Log.d("WANLEUNG", "swipeRight ");
+    }
+
+    public void swipeUp() {
+        // TODO Auto-generated method stub
+        Log.d("WANLEUNG", "swipeUp ");
+    }
+
+    public void onDestroy() {
+        this.mKeyData.close();
+    }
 }
