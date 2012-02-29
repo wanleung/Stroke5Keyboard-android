78179762e77e1e156c16da98dbd3198aac607818
Wan Leung Wong <wanleung@linkomnia.com>
2012-03-01 00:52:16 +0800

    a working code on the Chinese Punctuation typing, function key handling, and stroke mode handling testing

--- src/com/linkomnia/android/StrokeFiveKeyboard/StrokeFiveKeyboard.java
+++ src/com/linkomnia/android/StrokeFiveKeyboard/StrokeFiveKeyboard.java
@@ -44,6 +44,10 @@ public class StrokeFiveKeyboard extends InputMethodService implements
 
     private Stroke5KeyBoard mStroke5;
     private Stroke5Table mKeyData;
+    
+    private boolean strokemode;
+    private char [] charbuffer = new char[5];
+    private int strokecount = 0;
 
     @Override
     public void onCreate() {
@@ -65,16 +69,55 @@ public class StrokeFiveKeyboard extends InputMethodService implements
                 null);
         mInputView.setOnKeyboardActionListener(this);
         mInputView.setKeyboard(mStroke5);
+        strokemode = true;
         return mInputView;
     }
 
+    public void onStartInput(EditorInfo attribute, boolean restarting) {
+        super.onStartInput(attribute, restarting);
+        Log.d("WANLEUNG", "onStartInput");
+        strokemode = true;
+        this.stroktreset();
+        //this.mInputView.closing();
+    }
+    
+    public void onFinishInput() {
+        super.onFinishInput();
+        Log.d("WANLEUNG", "onFinishInput");
+    }
+    
+    public void onStartInputView(EditorInfo attribute, boolean restarting) {
+        super.onStartInputView(attribute, restarting);
+        Log.d("WANLEUNG", "onStartInputView");
+    }
+    
+    public void onUpdateSelection(int oldSelStart, int oldSelEnd,
+            int newSelStart, int newSelEnd,
+            int candidatesStart, int candidatesEnd) {
+        super.onUpdateSelection(oldSelStart, oldSelEnd, newSelStart, newSelEnd, candidatesStart, candidatesEnd);
+        Log.d("WANLEUNG", "onUpdateSelection");
+    }
+    
+    public void onDisplayCompletions(CompletionInfo[] completions) {
+        Log.d("WANLEUNG", "onDisplayCompletions");
+    }
+    
     public void onKey(int primaryCode, int[] keyCodes) {
         // TODO Auto-generated method stub
-        Log.d("WANLEUNG", "onkey " + primaryCode + " " + keyCodes.toString());
-        ArrayList<String> list = mKeyData.searchRecord(".n.");
-        Log.d("WANLEUNG", list.get(0) + "");
+        //Log.d("WANLEUNG", "onkey " + primaryCode + " " + keyCodes.toString());
+        //ArrayList<String> list = mKeyData.searchRecord(".n.");
+        //Log.d("WANLEUNG", list.get(0) + "");
+        //InputConnection ic = getCurrentInputConnection();
+        //ic.commitText(list.get(0), 1);
+        if (this.strokemode) {
+            this.handleStrokeMode(primaryCode);
+        }
     }
-
+    
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        Log.d(INPUT_METHOD_SERVICE, "onKeyDown");
+        return true;
+    }
     public void onPress(int primaryCode) {
         // TODO Auto-generated method stub
         Log.d("WANLEUNG", "onPress " + primaryCode);
@@ -114,4 +157,98 @@ public class StrokeFiveKeyboard extends InputMethodService implements
     public void onDestroy() {
         this.mKeyData.close();
     }
+    
+    private void stroktreset() {
+        this.charbuffer = new char[5];
+        this.strokecount = 0;
+    }
+    
+    private void handleStrokeMode(int keyCode){
+        switch (keyCode) {
+            case -5: {
+                this.handleBackspace();
+                break;
+            }
+            case 65292:
+            case 12290:
+            case 12289:
+            case 65281:
+            case 65311:
+            case 12288: {
+                this.handleChinesePunctuation(keyCode);
+                break;
+            }
+            case 3000:
+            case 3001:
+            case 3002:
+            case 3003: {
+                break;
+            }
+            default: {
+                this.typingStroke(keyCode);
+                break;
+            }
+        }
+    }
+    
+    private void typingStroke(int keycode) {
+        char c;
+        switch (keycode) {
+            case 1001: {
+                c = 'm';
+                break;
+            }
+            case 1002: {
+                c = '/';
+                break;
+            }
+            case 1003: {
+                c = ',';
+                break;
+            }
+            case 1004: {
+                c = '.';
+                break;
+            }
+            case 1005: {
+                c = 'n';
+                break;
+            }
+            default: {
+                c = '*';
+            }
+        }
+        this.charbuffer[this.strokecount++] = c;
+        Log.d("WANLEUNG", this.mKeyData.searchRecord(new String(this.charbuffer,0,this.strokecount)).get(0));
+    }
+    
+    private void handleBackspace() {
+        if (this.strokecount > 1) {
+            this.strokecount -= 1;
+            //getCurrentInputConnection().setComposingText(mComposing, 1);
+            //updateCandidates();
+        } else if (this.strokecount > 0) {
+            this.stroktreset();
+            //getCurrentInputConnection().commitText("", 0);
+            //updateCandidates();
+        } else {
+            keyDownUp(KeyEvent.KEYCODE_DEL);
+        }
+    }
+    
+    private void handleChinesePunctuation(int keycode) {
+        this.stroktreset();
+        InputConnection ic = getCurrentInputConnection();        
+        char c = (char)keycode;
+        String str = String.format("%c", c);
+        ic.commitText(str, 1);
+    }
+    
+    private void keyDownUp(int keyEventCode) {
+        getCurrentInputConnection().sendKeyEvent(
+                new KeyEvent(KeyEvent.ACTION_DOWN, keyEventCode));
+        getCurrentInputConnection().sendKeyEvent(
+                new KeyEvent(KeyEvent.ACTION_UP, keyEventCode));
+    }
+    
 }
