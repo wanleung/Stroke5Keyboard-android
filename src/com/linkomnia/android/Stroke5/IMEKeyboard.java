f1bcb65047a99999189cf60b54b2a346bce48ef4
Wan Leung Wong <wanleung@linkomnia.com>
2012-03-14 03:05:05 +0800

    add Classes IMEKeyboard and IMESwitch for refactoring

---
+++ src/com/linkomnia/android/Stroke5/IMEKeyboard.java
@@ -0,0 +1,56 @@
+/*
+    Stroke5 Chinese Input Method for Android
+    Copyright (C) 2012 LinkOmnia Ltd.  
+
+    Author: Wan Leung Wong (wanleung@linkomnia.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+package com.linkomnia.android.Stroke5;
+
+import android.content.Context;
+import android.inputmethodservice.Keyboard;
+
+public class IMEKeyboard extends Keyboard {
+    
+    static final int KEYCODE_CAPLOCK = -200;
+    static final int KEYCODE_MODE_CHANGE_CHAR = -300;
+    
+    private boolean isCapLock;
+    
+    public IMEKeyboard(Context context, int xmlLayoutResId) {
+        super(context, xmlLayoutResId);
+        this.isCapLock = false;
+        this.setShifted(false);
+    }
+
+    public IMEKeyboard(Context context, int layoutTemplateResId,
+            CharSequence characters, int columns, int horizontalPadding) {
+        super(context, layoutTemplateResId, characters, columns,
+                horizontalPadding);
+        this.isCapLock = false;
+        this.setShifted(false);
+    }
+    
+    public boolean isCapLock() {
+        return this.isCapLock;
+    }
+    
+    public void setCapLock(boolean b) {
+        this.isCapLock = b;
+        this.setShifted(b);
+    }
+}
---
+++ src/com/linkomnia/android/Stroke5/IMESwitch.java
@@ -0,0 +1,143 @@
+/*
+    Stroke5 Chinese Input Method for Android
+    Copyright (C) 2012 LinkOmnia Ltd.  
+
+    Author: Wan Leung Wong (wanleung@linkomnia.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+package com.linkomnia.android.Stroke5;
+
+import com.linkomnia.android.StrokeFiveKeyboard.R;
+
+import android.content.Context;
+import android.inputmethodservice.Keyboard;
+
+public class IMESwitch {
+    
+    private Context ctx;
+    
+    private IMEKeyboard currentKeyboard;
+    private IMEKeyboard englishKeyboard;
+    private IMEKeyboard enSymbolNumberKeyboard;
+    private IMEKeyboard enSymoblShiftKeyboard;
+    private IMEKeyboard chineseKeyboard;
+    
+    private boolean isFromChinese;
+    
+    public IMESwitch(Context ctx) {
+        this.ctx = ctx;
+        this.englishKeyboard = new IMEKeyboard(this.ctx, R.xml.qwert);
+        this.enSymbolNumberKeyboard = new IMEKeyboard(this.ctx, R.xml.symbols_en);
+        this.enSymoblShiftKeyboard = new IMEKeyboard(this.ctx, R.xml.symbols_ch);
+        this.chineseKeyboard = new IMEKeyboard(this.ctx, R.xml.stroke5);
+    }
+    
+    public void init() {
+        if (this.currentKeyboard == null) {
+            this.currentKeyboard = this.chineseKeyboard;
+        } else {
+            
+        }
+    }
+    
+    public Keyboard getCurrentKeyboard() {
+        return this.currentKeyboard;
+    }
+    
+    public boolean isChinese() {
+        return this.chineseKeyboard == this.currentKeyboard;
+    }
+
+    public boolean isEnglish() {
+        return this.englishKeyboard == this.currentKeyboard;
+    }
+
+    public boolean isSymbol() {
+        return this.enSymbolNumberKeyboard == this.currentKeyboard;
+    }
+
+    public boolean isPunctuation() {
+        return this.enSymoblShiftKeyboard == this.currentKeyboard;
+    }
+    
+    public boolean isNotCharKeyboard() {
+        return (this.isSymbol() || this.isPunctuation());
+    }
+       
+    public boolean handleKey(int keyCode) {
+        boolean result = false;
+        switch (keyCode) {
+            case IMEKeyboard.KEYCODE_CAPLOCK: {
+                this.currentKeyboard.setCapLock(! this.currentKeyboard.isCapLock());
+                break;
+            }
+            case IMEKeyboard.KEYCODE_SHIFT: {
+                this.currentKeyboard.setShifted(!(this.currentKeyboard.isCapLock() || this.currentKeyboard.isShifted()));
+                if (this.isNotCharKeyboard()) {
+                    this.switchBetweenSymbolShift();
+                }
+                result = true;
+                break;
+            }
+            case IMEKeyboard.KEYCODE_MODE_CHANGE_CHAR: {
+                this.switchToCharKeyboard();
+                result = true;
+                break;
+            }
+            case IMEKeyboard.KEYCODE_MODE_CHANGE: {
+                this.switchToSymbol();
+                result = true;
+                break;
+            }
+            default: {
+                result = false;
+            }
+        }
+        return result;
+    }
+    
+    public void switchToCharKeyboard() {
+        if (this.isNotCharKeyboard()) {
+            if (this.isFromChinese) {
+                this.currentKeyboard = this.chineseKeyboard;
+            } else {
+                this.currentKeyboard = this.englishKeyboard;
+            }
+        } else {
+            if (this.isChinese()) {
+                this.currentKeyboard = this.englishKeyboard;
+            } else {
+                this.currentKeyboard = this.chineseKeyboard;
+            }
+        }
+    }
+    
+    public void switchToSymbol() {
+        this.isFromChinese = this.isChinese();
+        this.currentKeyboard = this.enSymbolNumberKeyboard;            
+    }
+    
+    public void switchBetweenSymbolShift() {
+        if (this.currentKeyboard.isShifted()) {
+            this.currentKeyboard = this.enSymoblShiftKeyboard;
+            this.currentKeyboard.setCapLock(true);
+        } else {
+            this.currentKeyboard = this.enSymbolNumberKeyboard;
+            this.currentKeyboard.setCapLock(false);
+        }
+    }
+}
